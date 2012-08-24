9ff4991202607014eaf46ef39ed9bca49260b7a1
egaburov <egaburov@dds.nl>
2012-08-24 15:26:15 +0200

    added tabfunc.C

---
+++ bp3lib/tabfunc.C
@@ -0,0 +1,10 @@
+#include "tabfunc.h"
+
+template<>
+TabFuncTable<double> TabFunc<double>::t = TabFuncTable<double>();
+
+template<>
+TabFuncTable<float> TabFunc<float>::t = TabFuncTable<float>();
+
+
+
